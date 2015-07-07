5228426c956bb871dd2e1d458d4cc04e7bfd47fc
Luc Awater <lucawater@gmail.com>
2015-07-07 15:25:07 +0200

    tweak content field names

--- content/themes/elements/content/hero.php
+++ content/themes/elements/content/hero.php
@@ -1,47 +1,47 @@
 <?php
 // Options (variables)
-$hero_o_c_textColor = get_sub_field( 'hero_o_c_textColor' );
+$o_b_textColor = get_sub_field( 'hero_o_b_textColor' );
 
 // Content (variables)
-$hero_banner = get_sub_field( 'hero_banner' );
+$banner = get_sub_field( 'hero_banner' );
 
-$hero_c_image = get_sub_field( 'hero_c_image' );
-$hero_c_title = get_sub_field( 'hero_c_title' );
-$hero_c_text = preg_replace( '/<p>/', '<p class="' . $text_class . '">', get_sub_field( 'hero_c_text' ) );
+$b_image = get_sub_field( 'hero_b_image' );
+$b_title = get_sub_field( 'hero_b_title' );
+$b_text = preg_replace( '/<p>/', '<p class="is-' . $o_b_textColor . '">', get_sub_field( 'hero_b_text' ) );
 
 // Output
 echo '<section class="hero has-no-pad">';
-  
+
   // Hero background image
-  if( $hero_banner ): 
+  if( $banner ):
     echo
     '<div class="hero-banner is-stretched-wrapper">
-      <img class="is-stretched-object" src="' . $hero_banner['sizes']['large'] . '" width="' . $hero_banner['width'] . '" height="' . $hero_banner['height'] . '">
+      <img class="is-stretched-object" src="' . $banner['sizes']['large'] . '" width="' . $banner['width'] . '" height="' . $banner['height'] . '">
     </div>';
   endif;
-  
+
   // Hero content container
-  if( $hero_c_image || $hero_c_title || $hero_c_text ):
+  if( $b_image || $b_title || $b_text ):
     echo '<div class="section-body">';
-      
+
       // Logo
-      if( $hero_c_image ):
-        echo '<img src="' . $hero_c_image['sizes']['medium'] . '" width="' . $hero_c_image['width'] . '" height="' .   $hero_c_image['height'] . '">';
+      if( $b_image ):
+        echo '<img src="' . $b_image['sizes']['medium'] . '" width="' . $b_image['width'] . '" height="' .   $b_image['height'] . '">';
       endif;
-      
+
       // Title
-      if( $hero_c_title ):
-        echo '<h2 class="is-bold is-' . $hero_o_c_textColor . '">' . $hero_c_title . '</h2>';
+      if( $b_title ):
+        echo '<h2 class="is-bold is-' . $o_b_textColor . '">' . $b_title . '</h2>';
       endif;
-      
+
       // Text
-      if( $hero_c_text ):
-        echo $hero_c_text;
+      if( $b_text ):
+        echo $b_text;
       endif;
-      
+
     echo '</div>';
   endif;
-  
+
 echo '</section>';
 
 // <a href="javascript:;" class="arrow arrow-scroll"><img src="<?php echo bloginfo( 'template_directory' ) . /img/arrow.svg"></a>
--- content/themes/elements/content/slider.php
+++ content/themes/elements/content/slider.php
@@ -1,45 +1,45 @@
 <?php
 // Options
-$slider_o_menu = get_sub_field( 'slider_o_menu' );
+$o_menu = get_sub_field( 'slider_o_menu' );
 
-$slider_o_h_pos = get_sub_field( 'slider_o_h_pos' );
-$slider_o_h_align = get_sub_field( 'slider_o_h_align' );
+$o_h_pos = get_sub_field( 'slider_o_h_pos' );
+$o_h_align = get_sub_field( 'slider_o_h_align' );
 
 // Content
-$slider_h_title = get_sub_field( 'slider_h_title' );
-$slider_h_text = preg_replace( '/<p>/', '<p class="s-4 columns is-aligned-' . $slider_o_h_align . '">', get_sub_field( 'slider_h_text' ) );
-$slider_c_images = get_sub_field( 'slider_c_images' );
+$h_title = get_sub_field( 'slider_h_title' );
+$h_text = preg_replace( '/<p>/', '<p class="s-4 columns is-aligned-' . $o_h_align . '">', get_sub_field( 'slider_h_text' ) );
+$b_images = get_sub_field( 'slider_b_images' );
 
 // Output
-echo '<section class="slider' . (( $slider_o_menu == true ) ? ' has-anchor" id="anchor-' . $i_anchor : "") . '">';
-  
+echo '<section class="slider' . (( $o_menu == true ) ? ' has-anchor" id="anchor-' . $i_anchor : "") . '">';
+
   // Gallery header
-  if( $slider_h_title || $slider_h_text ): 
+  if( $h_title || $h_text ):
     echo
     '<div class="section-header row">
-      <h2 class="s-4 columns is-aligned-' . $slider_o_h_align . '">' . $slider_h_title . '</h2>
-      $slider_h_text
+      <h2 class="s-4 columns is-aligned-' . $o_h_align . '">' . $h_title . '</h2>
+      ' . $h_text . '
     </div>';
   endif;
-  
+
   // Gallery content
-  if( $slider_c_images ):
+  if( $b_images ):
     echo '<div class="section-body row">';
-      
+
       // The images
       echo '<ul class="slider-images s-4 columns">';
-        foreach( $slider_c_images as $image ):
+        foreach( $b_images as $image ):
           echo '<li><img src="' . $image['sizes']['large'] . '" width="' . $image['width'] . '" height="' . $image['height'] . '"></li>';
         endforeach;
       echo '</ul>';
-      
+
       // The bullets
       echo '<ul class="slider-bullets">';
-        foreach( $slider_c_images as $image ):
+        foreach( $b_images as $image ):
           echo '<li><i></i></li>';
         endforeach;
       echo '</ul>';
-    
+
       // The controls
       if(! $detect->isMobile() ):
         echo
@@ -48,9 +48,9 @@ echo '<section class="slider' . (( $slider_o_menu == true ) ? ' has-anchor" id="
           <a class="slider-next arrow arrow-right" href="javascript:;"><i></i></a>
         </div>';
       endif;
-      
+
     echo '</div>';
   endif;
-  
+
 echo '</section>';
 ?>
