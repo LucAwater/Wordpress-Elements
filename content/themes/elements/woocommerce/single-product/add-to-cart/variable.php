933baeb0cb8a43f9eb67f17062aaa3b886b7d6fa
Luc Awater <lucawater@gmail.com>
2015-07-16 14:24:46 +0200

    add woocommerce template files

---
+++ content/themes/elements/woocommerce/single-product/add-to-cart/variable.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Variable product add to cart
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+global $woocommerce, $product, $post;
+?>
+
+<script type="text/javascript">
+    var product_variations_<?php echo $post->ID; ?> = <?php echo json_encode( $available_variations ) ?>;
+</script>
+
+<?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
+
+<form class="variations_form cart" method="post" enctype='multipart/form-data' data-product_id="<?php echo $post->ID; ?>" data-product_variations="<?php echo esc_attr( json_encode( $available_variations ) ) ?>">
+
+  <!-- Variations -->
+  <?php if ( ! empty( $available_variations ) ) : ?>
+    <?php $loop = 0; foreach ( $attributes as $name => $options ) : $loop++; ?>
+      <div class="variations">
+        <fieldset>
+          <?php
+          if ( is_array( $options ) ) {
+
+            if ( empty( $_POST ) )
+              $selected_value = ( isset( $selected_attributes[ sanitize_title( $name ) ] ) ) ? $selected_attributes[ sanitize_title( $name ) ] : '';
+            else
+              $selected_value = isset( $_POST[ 'attribute_' . sanitize_title( $name ) ] ) ? $_POST[ 'attribute_' . sanitize_title( $name ) ] : '';
+              //echo   $selected_value;
+              // Get terms if this is a taxonomy - ordered
+              if ( taxonomy_exists( sanitize_title( $name ) ) ) {
+                $terms = get_terms( sanitize_title($name), array('menu_order' => 'ASC') );
+
+                foreach ( $terms as $term ) {
+                  if ( ! in_array( $term->slug, $options ) ) continue;
+
+                    echo '<label>';
+                      echo '<input type="radio" value="' . strtolower($term->slug) . '" ' . checked( strtolower ($selected_value), strtolower ($term->slug), false ) . ' id="'. esc_attr( sanitize_title($name) ) .'" name="attribute_'. sanitize_title($name).'">' . apply_filters( 'woocommerce_variation_option_name', $term->name );
+                    echo '</label>';
+                  }
+                } else {
+
+                foreach ( $options as $option ) {
+                  echo '<label>';
+                    echo '<input type="radio" value="' .esc_attr( sanitize_title( $option ) ) . '" ' . checked( sanitize_title( $selected_value ), sanitize_title( $option ), false ) . ' id="'. esc_attr( sanitize_title($name) ) .'" name="attribute_'. sanitize_title($name).'">' . apply_filters( 'woocommerce_variation_option_name', $option );
+                  echo '</label>';
+                }
+              }
+            }
+            ?>
+          </fieldset> <?php
+          if ( sizeof($attributes) == $loop )
+            //echo '<a class="reset_variations" href="#reset">' . __( 'Clear selection', 'woocommerce' ) . '</a>';
+          ?>
+      </div>
+    <?php endforeach;?>
+
+    <?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+
+    <?php if ( $product->is_in_stock() ) : ?>
+
+      <!-- Variable price -->
+      <div class="single_variation"></div>
+
+      <div class="single_variation_wrap">
+        <?php do_action( 'woocommerce_before_single_variation' ); ?>
+
+        <button type="submit" class="single_add_to_cart_button product-type-variable button alt">Add to cart</button>
+
+        <input type="hidden" name="add-to-cart" value="<?php echo $product->id; ?>" />
+        <input type="hidden" name="product_id" value="<?php echo esc_attr( $post->ID ); ?>" />
+        <input type="hidden" name="variation_id" value="" />
+
+        <?php do_action( 'woocommerce_after_single_variation' ); ?>
+      </div>
+    <?php else: ?>
+
+      <p class="stock out-of-stock"><?php _e( 'Out of stock', 'woocommerce' ); ?></p>
+
+    <?php endif; ?>
+
+    <?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+
+  <?php else : ?>
+
+    <p class="stock out-of-stock"><?php _e( 'This product is currently out of stock and unavailable.', 'woocommerce' ); ?></p>
+
+  <?php endif; ?>
+
+</form>
+
+<?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
