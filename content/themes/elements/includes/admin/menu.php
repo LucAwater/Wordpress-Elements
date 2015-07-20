378ec34b13eba5c2cd2f88f12136b17502731c00
Luc Awater <lucawater@gmail.com>
2015-07-20 14:20:41 +0200

    reposition woocommerce in backend menu

--- content/themes/elements/includes/admin/menu.php
+++ content/themes/elements/includes/admin/menu.php
@@ -15,13 +15,14 @@ function menu_order( $menu_order ) {
   return array(
     'index.php', //Dashboard
     'separator1',
-    
+
     'upload.php', //Media
     'edit.php', //Posts
     'edit.php?post_type=page', //Pages
     'footer', //Footer information
+    'woocommerce',
     'separator-last',
-    
+
     'edit.php?post_type=acf-field-group', //Custom fields
     'options-general.php', //Settings
     'tools.php', //Tools
