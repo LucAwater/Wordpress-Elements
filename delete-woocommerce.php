c3dd42160a87c817afe118e1bf1b560b0ad0879c
Luc Awater <lucawater@gmail.com>
2015-07-20 10:56:00 +0200

    fix paths for woocommerce delete functionalitry

--- delete-woocommerce.php
+++ delete-woocommerce.php
@@ -25,8 +25,8 @@ function Delete($path){
 
 Delete('content/plugins/woocommerce');
 Delete('content/plugins/woocommerce-ajax-add-to-cart-for-variable-products');
-Delete('content/theme/elements/woocommerce');
-Delete('content/theme/elements/sass/woocommerce');
+Delete('content/themes/elements/woocommerce');
+Delete('content/themes/elements/sass/woocommerce');
 
 // Remove functions in files
 $string1 = "// Woocommerce includes
@@ -43,26 +43,14 @@ $contents1 = file_get_contents($file1);
 $contents1 = str_replace($string1, '', $contents1);
 file_put_contents($file1, $contents1);
 
-$string2 = '<div class="cart-contents">
-  <?php
-  $cart_total = WC()->cart->get_cart_total();
-  $cart_count = WC()->cart->cart_contents_count;
 
-  echo "<p>" . $cart_count . " – " . $cart_total . "</p>";
-  ?>
-</div>';
-$file2 = 'content/themes/elements/header.php';
-$contents2 = file_get_contents($file2);
-$contents2 = str_replace($string2, '', $contents2);
-file_put_contents($file2, $contents2);
-
-$string3 = "
+$string2 = "
 if ( class_exists('WooCommerce') ) {
   // Woocommere page shortcodes(see pages in backend)
   the_content();
 }";
-$file3 = 'content/themes/elements/page.php';
-$contents3 = file_get_contents($file3);
-$contents3 = str_replace($string3, '', $contents3);
-file_put_contents($file3, $contents3);
+$file2 = 'content/themes/elements/page.php';
+$contents2 = file_get_contents($file2);
+$contents2 = str_replace($string2, '', $contents2);
+file_put_contents($file2, $contents2);
 ?>
