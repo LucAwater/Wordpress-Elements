e2e7e029f28ca0f6cb5d77d7d0716404c28cfd60
Luc Awater <lucawater@gmail.com>
2015-10-29 12:32:51 +0100

    Introduce levels in styling
    No matter what the elements are, the padding is now taken care of in levels. Everything that is 1 (DOM) level below the body, gets the same padding. Same goes for everything 1 level below the main element.

--- content/themes/elements/content/text.php
+++ content/themes/elements/content/text.php
@@ -20,3 +20,26 @@ echo
   </div>
 </section>';
 ?>
+
+<?php
+// Options (variables)
+$o_menu = get_sub_field( 'text_o_menu' );
+
+$o_b_pos = get_sub_field( 'text_o_b_pos' );
+$o_b_align = get_sub_field( 'text_o_b_align' );
+
+// Content (variables)
+$b_title = get_sub_field( 'text_b_title' );
+$b_text = preg_replace( '/<p>/', '<p class="is-aligned-' . $o_b_align . '">', get_sub_field( 'text_b_text' ) );
+
+// Classes
+$class_section = 'text';
+$class_body = 'section-body is-pos-' . $o_b_pos;
+?>
+
+<section class="<?php echo $class_section; ?>">
+  <div class="<?php echo $class_body; ?>">
+    <h2><?php echo $b_title; ?></h2>
+    <div><?php echo $b_text; ?></div>
+  </div>
+</section>
