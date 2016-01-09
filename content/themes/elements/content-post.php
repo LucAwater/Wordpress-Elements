b8a4d632d6fbdb52712a6892a3e01372867fdafa
Luc Awater <lucawater@gmail.com>
2016-01-09 21:50:59 +0100

    Add permalink to post categories

--- content/themes/elements/content-post.php
+++ content/themes/elements/content-post.php
@@ -2,6 +2,7 @@
 // Content (variables)
 $title = get_the_title();
 $category = get_the_category( $post->ID );
+$category_link = get_category_link( $category[0]->term_id );
 $content = wpautop( get_the_content() );
 $permalink = get_the_permalink();
 $date = get_the_date();
@@ -10,7 +11,7 @@ $thumb = get_the_post_thumbnail( $post->ID, 'medium' );
 
 <li>
   <div>
-    <p class="post-category"><?php echo $category[0]->cat_name; ?></p>
+    <a href="<?php echo $category_link; ?>" class="post-category"><?php echo $category[0]->cat_name; ?></a>
     <a class="post-title" href="<?php echo $permalink; ?>"><h3><?php echo $title; ?></h3></a>
   </div>
 
